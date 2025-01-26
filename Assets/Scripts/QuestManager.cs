9f739789c366e99e044f488bfe7324511339d7d4
Andrey Orlikov <44662043+FlyingPug@users.noreply.github.com>
2025-01-26 19:03:08 +0700

    FIX WALLS

--- Assets/Scripts/QuestManager.cs
+++ Assets/Scripts/QuestManager.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Xml;
 using UnityEngine;
 
 public class Quest
@@ -27,22 +29,61 @@ public class Quest
 
 public class QuestManager : MonoBehaviour
 {
-    public List<Quest> quests = new List<Quest>();
+    public List<Quest> currentQuests = new();
+    public Dictionary<int, Quest> allQuests = new();
     public SubtitleManager subtitleManager;
 
     void Start()
     {
         subtitleManager = GetComponent<SubtitleManager>();
+        LoadQuests();
+    }
+
+    private void LoadQuests()
+    {
+        string filePath = Path.Combine(Application.dataPath, "quests.xml");
+        Debug.Log($"Attempting to load file: {filePath}");
+
+        if (File.Exists(filePath))
+        {
+            try
+            {
+                XmlDocument xmlDocument = new XmlDocument();
+                xmlDocument.Load(filePath);
+
+                XmlNodeList lines = xmlDocument.GetElementsByTagName("Day");
+                foreach (XmlNode line in lines)
+                {
+                    int id = int.Parse(line.Attributes["id"].Value);
+                    string name = line.Attributes["name"].Value;
+                    string description = line.Attributes["description"].Value;
+                    string subtitleText = line.Attributes["subtitleText"].Value;
+                    WeatherType weatherType = Enum.Parse<WeatherType>(line.Attributes["weatherType"].Value);
+
+                    allQuests.Add(id, new Quest(id, name, description, subtitleText));
+                }
+
+                Debug.Log("Subtitles loaded successfully.");
+            }
+            catch (XmlException e)
+            {
+                Debug.LogError($"Error parsing XML file: {e.Message}");
+            }
+        }
+        else
+        {
+            Debug.LogError("Failed to load the XML file.");
+        }
     }
 
     public void AddQuest(int id, string name, string description, string subtitleText)
     {
-        quests.Add(new Quest(id, name, description, subtitleText));
+        currentQuests.Add(new Quest(id, name, description, subtitleText));
     }
 
     public void CompleteQuest(int questId)
     {
-        Quest quest = quests.Find(q => q.id == questId);
+        Quest quest = currentQuests.Find(q => q.id == questId);
         if (quest != null && !quest.isCompleted)
         {
             quest.CompleteQuest();
@@ -53,7 +94,7 @@ public class QuestManager : MonoBehaviour
 
     public bool IsQuestCompleted(int questId)
     {
-        Quest quest = quests.Find(q => q.id == questId);
+        Quest quest = currentQuests.Find(q => q.id == questId);
         return quest != null && quest.isCompleted;
     }
 }
